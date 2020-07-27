778e7e1c0389de4c8fa57922568ed1f944c90d49
Peter Spackman <peterspackman@fastmail.com>
2020-07-27 10:00:33 +0800

    fixed kernel compiler defaulting to 'gcc' rather than

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 SHTNS="shtns-3.4.2"
 cd src
-./configure --enable-python --disable-openmp --prefix=${PREFIX}
+./configure --enable-python --disable-openmp --prefix=${PREFIX} --enable-kernel-compiler=${CC}
 make
 make install
